2fa7ac48008a869b33b090665d08780671603034
Tân <nguyenduytan280501@gmail.com>
2023-02-26 15:44:08 +0700

    tai layout

---
+++ src/routes/index.js
@@ -0,0 +1,17 @@
+import Home from "../page/Home";
+import Following from "../page/Following";
+import Profile from "../page/Profile";
+import Upload from "../page/Upload";
+
+const publicRoutes = [
+    { path: '/', component: Home},
+    { path: '/following', component: Following},
+    { path: '/profile', component: Profile},
+    { path: '/upload', component: Upload, layout: null},
+
+]
+
+const privateRoutes = [
+    
+];
+export { publicRoutes, privateRoutes}
